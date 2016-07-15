204b434f2fde2135add0993c88ebac34e698c856
dora <joy8963259@gmail.com>
2016-07-15 13:26:55 +0800

    First Version

---
+++ RoutePlan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RoutePlan.Startup))]
+namespace RoutePlan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
