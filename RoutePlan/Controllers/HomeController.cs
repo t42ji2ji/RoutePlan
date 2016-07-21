65f0a5847fe9cbfd447e0c9b37387673620608c3
dora <joy8963259@gmail.com>
2016-07-21 23:20:44 +0800

    AJAX

--- RoutePlan/Controllers/HomeController.cs
+++ RoutePlan/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using RoutePlan.ViewModels;
 
 namespace RoutePlan.Controllers
 {
@@ -12,6 +14,24 @@ namespace RoutePlan.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Index(string form_from,string form_to)
+        {
+            LocationViewModel lvm = new LocationViewModel();
+            
+            return View();
+        }
+
+        public ActionResult Update(List<LocationViewModel> lvm)
+        {
+            foreach(var item in lvm)
+            {
+                item.ID = "UPDATE";
+            }
+            return Json(lvm);
+        }
+
 
         public ActionResult About()
         {
@@ -23,8 +43,18 @@ namespace RoutePlan.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
-            return View();
+            return View(new ReportViewModel());
         }
+
+        [HttpPost]
+        public ActionResult Contact(string reportName)
+        {
+            ReportViewModel rv = new ReportViewModel();
+            rv.Name = reportName;
+            return View(rv);
+        }
+
+
+
     }
 }
