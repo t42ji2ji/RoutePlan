eb06441330cc42bff52b5ad5832b63d17708fcaf
dora <joy8963259@gmail.com>
2016-07-29 09:30:46 +0800

    文件

---
+++ RoutePlan/ViewModels/LocationViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace RoutePlan.ViewModels
+{
+    public class LocationViewModel
+    {
+        [DisplayName("ID")]
+        public string ID { set; get; }
+
+        [DisplayName("經度")]
+        public double longitude { set; get; }
+        
+
+        [DisplayName("緯度")]
+        public double latitude { set; get; }
+    }
+}
---
+++ RoutePlan/ViewModels/ReportViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace RoutePlan.ViewModels
+{
+    public class ReportViewModel
+    {
+
+        public string Name { set; get; }
+    }
+}
